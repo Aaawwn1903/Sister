07f122b2b331ffccec8711a1d4a8b9112eb6c30c
Aaawwn1903 <74972193+Aaawwn1903@users.noreply.github.com>
2023-07-06 23:46:47 +0800

    Add files via upload

---
+++ Latihan Docker/web/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="max-w-md mx-auto bg-white rounded p-8 shadow-md">
+        <img class="w-32 h-32 mx-auto rounded-full mb-4" src="https://pps.whatsapp.net/v/t61.24694-24/292871207_447564160263376_2529063888016971338_n.jpg?ccb=11-4&oh=01_AdRwLaTws0l0T1AHHdedM2lmOpG9tAQYrb_nyMxo-8tziQ&oe=64B3E2B0" alt="Profile Picture">
+        <h1 class="text-2xl font-bold text-center">Welcome to My Website</h1>
+        <p class="text-gray-600 text-center mt-4">Thank you for visiting. This is a sample PHP page served by Nginx.</p>
+        <p class="text-gray-600 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at lectus commodo, congue tellus at, bibendum nulla. Duis et lacus a neque venenatis auctor et vitae nunc.</p>
+        <a href="#" class="block bg-blue-500 hover:bg-blue-600 text-white rounded-lg px-4 py-2 mt-4 text-center">Learn More</a>
+    </div>
+</body>
+</html>
